1380513bd5390dfc0824db9f589156d26f563c6d
Oskar Gewalli <oskar@gewalli.se>
2013-10-13 08:23:30 +0200

    Should have tasks

---
+++ it_up/TaskTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+namespace Shake.It.Up
+{
+    [TestFixture]
+    public class TaskTests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.Fail("Not implemented");
+        }
+    }
+}
